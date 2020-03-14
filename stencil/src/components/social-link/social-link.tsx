c8918c5bb9754d34ac428c1d680a796da66d00d0
Simon Knittel <hallo@simonknittel.de>
2020-03-14 17:46:19 +0100

    Adjust icon handling of social-link

--- stencil/src/components/social-link/social-link.tsx
+++ stencil/src/components/social-link/social-link.tsx
@@ -10,18 +10,30 @@ export class SocialLink {
   @Prop() url: string
   @Prop() text: string
   @Prop() icon: string
-  @Prop() iconViewbox: string
-  @Prop() iconAddBackground: boolean
 
   render() {
+    const viewbox = {
+      'github-square': '448 512',
+      'twitter-square': '448 512',
+      'patreon': '512 512',
+      'stack-overflow': '384 512',
+      'xing-square': '448 512',
+      'linkedin': '448 512'
+    }
+
+    const addBackground = [
+      'patreon',
+      'stack-overflow'
+    ]
+
     return (
       <a href={this.url} target="_blank" rel="noopener">
         <span class={{
           icon: true,
-          'icon--add-background': this.iconAddBackground,
+          'icon--add-background': addBackground.indexOf(this.icon) >= 0,
           'icon--patreon': this.icon === 'patreon'
         }}>
-          <svg viewBox={"0 0 " + this.iconViewbox}>
+          <svg viewBox={"0 0 " + viewbox[this.icon]}>
             <use xlinkHref={getAssetPath(`./assets/${this.icon}-brands.svg`) + '#id'} />
           </svg>
         </span>
