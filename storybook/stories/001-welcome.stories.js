752c6f4bbad0ff4132ae42104f8421daaf24d56c
Simon Knittel <hallo@simonknittel.de>
2020-07-29 12:04:14 +0200

    Improve docs

--- storybook/stories/001-welcome.stories.js
+++ storybook/stories/001-welcome.stories.js
@@ -1,44 +1,23 @@
+import { markdownToHTML } from '../utils'
+
+import githubMarkdownCSS from '!!raw-loader!../github-markdown.css'
+
+import README from '../../README.md'
+import CHANGELOG from '../../CHANGELOG.md'
+import LICENSE from 'raw-loader!../../LICENSE'
+
 export default {
   title: 'SK Components/Welcome',
-  parameters: {
-    backgrounds: [
-      { name: 'white', value: '#fff5cc', default: true },
-    ]
-  },
 }
 
 export function Overview() {
-  return `
-  <sk-typography as="h1">Overview</sk-typography>
-
-  <sk-typography as="p">Proident excepteur qui tempor cillum aliqua nostrud aliquip esse reprehenderit. Nisi mollit exercitation qui officia sunt deserunt. Nisi fugiat voluptate ex culpa quis. In tempor irure ipsum irure ipsum dolore ad enim labore adipisicing non reprehenderit ipsum commodo.</sk-typography>
-  <sk-typography as="p">Ex veniam commodo culpa excepteur sunt consectetur mollit aliquip ad qui reprehenderit. Laboris ex excepteur sunt sunt commodo ut commodo sit ea consequat. Laborum do quis pariatur proident eu incididunt magna commodo sit aliquip aliquip.</sk-typography>
-  `
-}
-
-export function Installation() {
-  return `
-  <sk-typography as="h1">Installation</sk-typography>
-
-  <sk-typography as="p">Proident excepteur qui tempor cillum aliqua nostrud aliquip esse reprehenderit. Nisi mollit exercitation qui officia sunt deserunt. Nisi fugiat voluptate ex culpa quis. In tempor irure ipsum irure ipsum dolore ad enim labore adipisicing non reprehenderit ipsum commodo.</sk-typography>
-  <sk-typography as="p">Ex veniam commodo culpa excepteur sunt consectetur mollit aliquip ad qui reprehenderit. Laboris ex excepteur sunt sunt commodo ut commodo sit ea consequat. Laborum do quis pariatur proident eu incididunt magna commodo sit aliquip aliquip.</sk-typography>
-  `
+  return `<style>${githubMarkdownCSS}</style><div class="markdown-body">${markdownToHTML(README)}</div>`
 }
 
 export function Changelog() {
-  return `
-  <sk-typography as="h1">Changelog</sk-typography>
-
-  <sk-typography as="p">Proident excepteur qui tempor cillum aliqua nostrud aliquip esse reprehenderit. Nisi mollit exercitation qui officia sunt deserunt. Nisi fugiat voluptate ex culpa quis. In tempor irure ipsum irure ipsum dolore ad enim labore adipisicing non reprehenderit ipsum commodo.</sk-typography>
-  <sk-typography as="p">Ex veniam commodo culpa excepteur sunt consectetur mollit aliquip ad qui reprehenderit. Laboris ex excepteur sunt sunt commodo ut commodo sit ea consequat. Laborum do quis pariatur proident eu incididunt magna commodo sit aliquip aliquip.</sk-typography>
-  `
+  return `<style>${githubMarkdownCSS}</style><div class="markdown-body">${markdownToHTML(CHANGELOG)}</div>`
 }
 
 export function License() {
-  return `
-  <sk-typography as="h1">License</sk-typography>
-
-  <sk-typography as="p">Proident excepteur qui tempor cillum aliqua nostrud aliquip esse reprehenderit. Nisi mollit exercitation qui officia sunt deserunt. Nisi fugiat voluptate ex culpa quis. In tempor irure ipsum irure ipsum dolore ad enim labore adipisicing non reprehenderit ipsum commodo.</sk-typography>
-  <sk-typography as="p">Ex veniam commodo culpa excepteur sunt consectetur mollit aliquip ad qui reprehenderit. Laboris ex excepteur sunt sunt commodo ut commodo sit ea consequat. Laborum do quis pariatur proident eu incididunt magna commodo sit aliquip aliquip.</sk-typography>
-  `
+  return `<pre>${LICENSE}</pre>`
 }
---
+++ storybook/utils.js
@@ -0,0 +1,19 @@
+import unified from 'unified'
+import markdown from 'remark-parse'
+import remark2rehype from 'remark-rehype'
+import html from 'rehype-stringify'
+import sanitize from 'rehype-sanitize'
+import gh from 'hast-util-sanitize/lib/github'
+
+export function markdownToHTML(rawMarkdown) {
+  const sanitizeOptions = gh
+
+  const htmlString = unified()
+    .use(markdown)
+    .use(remark2rehype)
+    .use(html)
+    .use(sanitize, sanitizeOptions)
+    .processSync(rawMarkdown).contents
+
+  return htmlString
+}
